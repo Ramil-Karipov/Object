4d97d7ea4dcc962227acc705d0dd99204a2ff82a
Ramil-Karipov <omil8@yandex.ru>
2023-11-09 09:46:44 +0500

    designerFiniSh1

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,12 @@
 public class Book {
     private String title;
     private int publicationYear;
+    private Author author;
 
-    public Book(String title, int publicationYear) {
+    public Book(String title, int publicationYear, Author author) {
         this.title = title;
         this.publicationYear = publicationYear;
+        this.author = author;
     }
 
     public String getTitle() {
@@ -13,6 +15,10 @@ public class Book {
 
     public int getPublicationYear() {
         return this.publicationYear;
+    }
+
+    public Author getAuthor() {
+        return this.author;
     }
 
     public void setPublicationYear(int publicationYear) {
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         Author eSeNiN = new Author("Сергей", "Есенин");
-        Book whiteBirch = new Book("Белая берёза", 1914);
+        Book whiteBirch = new Book("Белая берёза", 1914, eSeNiN);
         System.out.println("Автор: " + eSeNiN.getLastName() + " " + eSeNiN.getFirstName());
         System.out.println("Произведение: " + whiteBirch.getTitle() + " " + whiteBirch.getPublicationYear());
 
         Author mayaKovSky = new Author("Владимир", "Маяковский");
-        Book listeUp = new Book("Послушайте!", 1913);
+        Book listeUp = new Book("Послушайте!", 1913, mayaKovSky);
         System.out.println("Автор: " + mayaKovSky.getLastName() + " " + mayaKovSky.getFirstName());
         System.out.println("Произведение: " + listeUp.getTitle() + " " + listeUp.getPublicationYear());
         listeUp.setPublicationYear(2023);
